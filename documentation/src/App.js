77e4564e4bd409da0e6768eb89a5ae0ad01f012c
Kacper Szarkiewicz <kacperszarkiewicz@MacBook-Pro-Kacper.local>
2021-12-04 21:25:48 +0100

    add some sectons on documentation

--- documentation/src/App.js
+++ documentation/src/App.js
@@ -9,8 +9,20 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <Section title={<FormattedMessage id="howItWorks.title" defaultMessage="How it works?" />} imgSrc={img1} imgTitle={'Create a Google Spreadsheet'} description={'Create a Google Spreadsheet and fill it with wanted data'}/>
-      <Section title={undefined} imgSrc={img2} imgTitle={'Name the spreadsheet'} description={'You can name the spreadsheet'}/>
+      <Section title={<FormattedMessage id="howItWorks.title" defaultMessage="How it works?" />} imgSrc={img1} imgTitle={'Create a Google Spreadsheet'} description={'Create a Google Spreadsheet and fill it with wanted data. In "A" column place keys and in the "B" column type the relevant translation.'}/>
+      <div className="warning"><span className="important">!</span>Remeber to <span className="strongWarning">place the keys in <span className="veryStrongWarning">A</span>column and <span className="strongWarning">translations in the <span className="veryStrongWarning">B</span> column</span> </span></div>
+      <Section title={undefined} imgSrc={img2} imgTitle={'Name the spreadsheets'} description={'You can name the spreadsheet'}/>
+      <pre className="codesnippet__pre">
+      <code className="codesnippet__code">
+        <div className="comment">//Add this config in your plugins in webpack.config.js</div>
+        <div>new GoogleSheetToJson({`{`} </div>
+        <div className="space">spreadsheetId:<span className="string"> 'mySecretSpreadsheetId'</span>,</div>
+        <div className="space">output:  path.join(__dirname, <span className="string">'yourTranslations.json'</span>),</div>
+        <div className="space">apiKey: yourApiKey,</div>
+        <div className="space">isDev: isEnvDevelopment</div>
+        <div>{`}`})</div>
+      </code>
+      </pre>
     </div>
 
   );
--- documentation/src/components/ImageSection/ImageSection.jsx
+++ documentation/src/components/ImageSection/ImageSection.jsx
@@ -1,10 +1,10 @@
 import {Image} from '../Image'
 
 const ImageSection = ({imgSrc, title, description}) => (
-    <div>
+    <div >
         <Image src={imgSrc}/>
         <h3>{title}</h3>
-        <p>{description}</p>
+        <p className="ImageSection">{description}</p>
     </div>
 )
 
